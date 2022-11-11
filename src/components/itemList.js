8aa8118c9c4290fb60727f2c21ed10d67fcc52ef
Simon Pineda <simonpineda0521@gmail.com>
2022-11-10 21:34:49 -0500

    Agregue el loading de los productos, pero no mucho mas

--- src/components/itemList.js
+++ src/components/itemList.js
@@ -1,9 +1,36 @@
-function ItemList({greeting}){
+import { useState } from "react";
+import Items from './items.js';
+const ProductsTest =  [{
+    id: "001",
+    title: "logo",
+    description: "A custom logo for your brand, made according to your preferences",
+    price: "US$99",
+    pictureUrl: null,
+    stock:12,
+},
+{
+    id: "002",
+    title: "logo + website",
+    description: "A custom logo for your brand, and a website where you can expose your brand",
+    price: "US$199",
+    pictureUrl: null,
+    stock: 6,
+}]
+function ItemList(){
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
+    const productsFromApi = new Promise((resolve) => {  setTimeout(()=>{resolve(ProductsTest)}, 10000)//en este espacio estaria el lamado a la api, el cual por cada dato que llegue (objetos) lo metera en el array allProducts
+    })
+    productsFromApi.then((data) => {
+        setProducts(data)
+    }).then((data)  => setLoading(!!data))
     return(
     <section className="productContainer">
-            <h1>{greeting}</h1>
-        </section>
+            {loading && <div className="loading"><div class="lds-dual-ring"></div></div>}
+            {products.map((product) => {
+            <Items props={product}></Items>
+            })}
+    </section>
     )
 }
-
 export default ItemList;
---
+++ src/components/items.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+function Items({id, title, description, price, pictureUrl, stock}){
+    
+    return(
+        
+    <div>
+
+    </div>
+    )
+}
+export default Items;
