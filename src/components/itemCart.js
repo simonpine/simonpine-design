bc4879f1554ecf3437599a6afd505b916200dbaf
simon pineda <simonpineda0521@gmail.com>
2023-04-20 21:24:05 -0500

    the cart design now works on tablets

--- src/components/itemCart.js
+++ src/components/itemCart.js
@@ -1,10 +1,10 @@
-import {Link} from 'react-router-dom'
-import {ContextCart} from '../context/cartContext'
-function ItemCartDetail( { porps, restart } ){
+import { Link } from 'react-router-dom'
+import { ContextCart } from '../context/cartContext'
+function ItemCartDetail({ porps, restart }) {
     const price = Math.round(porps.price * porps.number)
-    return(
+    return (
         <ContextCart.Consumer>
-        {( { deleteItemAllS, plus, less } ) =>{
+            {({ deleteItemAllS, plus, less }) => {
                 function deleteAll() {
                     deleteItemAllS(porps.id)
                     restart()
@@ -17,26 +17,26 @@ function ItemCartDetail( { porps, restart } ){
                     less(porps, porps.number)
                     restart()
                 }
-            return(
-                <div className="ItemCartDetail">
-                <Link to={{pathname:`/Store/item/${porps.id}`}}  className="imgCartItemCon">
-                    <img className="imgCartItem" src={porps.pictureUrl} />
-                </Link> 
-                <h2 className="titleCartItem"> { porps.title}</h2>
-                <h2 className="titleCartItem h">${price}</h2>
-                <div>
-                    <div className="allButtonsCart">
-                        <button className="clear r" onClick={() => {deleteAll()}}>Delete</button>
-                        <div className="moreLessBut">
-                            <button className="butCart" onClick={() => {lessAction()}}>-</button>
-                            <h3 className="actualCart h">{porps.number}</h3>
-                            <button className="butCart" onClick={() => {plusAction()}}>+</button>
+                return (
+                    <div className="ItemCartDetail">
+                        <Link to={{ pathname: `/Store/item/${porps.id}` }} className="imgCartItemCon">
+                            <img className="imgCartItem" src={porps.pictureUrl} />
+                        </Link>
+                        <h2 className="titleCartItem"> {porps.title}</h2>
+                        <h2 className="titleCartItem h priceCart">${price}</h2>
+                        <div className='but'>
+                            <div className="allButtonsCart">
+                                <button className="clear r deleteBut" onClick={() => { deleteAll() }}>Delete</button>
+                                <div className="moreLessBut">
+                                    <button className="butCart" onClick={() => { lessAction() }}>-</button>
+                                    <h3 className="actualCart h">{porps.number}</h3>
+                                    <button className="butCart" onClick={() => { plusAction() }}>+</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            )
-        }}
+                )
+            }}
         </ContextCart.Consumer>
     )
 }
